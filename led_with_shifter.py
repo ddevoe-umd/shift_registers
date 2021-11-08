17ab747773e33733ba0c8e75b3098d7b49dad26a
ddevoe-umd <84353435+ddevoe-umd@users.noreply.github.com>
2021-11-08 14:30:42 -0500

    Update led_with_shifter.py

--- led_with_shifter.py
+++ led_with_shifter.py
@@ -1,15 +1,16 @@
 import time
 from led_display import LEDdisplay
 
+# Simple demonstration of the LEDdisplay class.
 # Note that we don't need RPi.GPIO here since all the I/O
 # is done through the LEDdisplay class (we do however need
-# to define the GPIO pins here, since LEDdisplay is
-# pin-agnostic)
+# to define the GPIO pins, since LEDdisplay is
+# pin-agnostic).
 
-dataPin, latchPin, clockPin = 16, 12, 6
+dataPin, latchPin, clockPin = 23, 24, 25
 
 # Pick a number sequence
-sequence = [8, 6, 7, 5, 3, 0, 9];
+sequence = [8, 6, 7, 5, 3, 0, 9]
 
 theLEDdisplay= LEDdisplay(dataPin, latchPin, clockPin)
 
